20e824c7d17d054028a29a735572a0057dcc2e06
Thiago <tbgdmelo@gmail.com>
2021-04-28 23:36:07 -0400

    Atividade 1 js

---
+++ JS1/script.js
@@ -0,0 +1,14 @@
+let x=1;
+while (x<11){
+    document.write("<table border='1'>");
+    document.write("<tbody><tr><th colspan='2'>Produto de "+x+"</th><tr>");
+    document.write("<tr>");
+    let y=1;
+    while(y<11){
+        document.write("<td>"+x+"x"+y+"</td>");
+        document.write("<td>"+(x*y)+"</td>");
+        document.write("</tr>");
+        y++;
+    }
+    x++;
+}
