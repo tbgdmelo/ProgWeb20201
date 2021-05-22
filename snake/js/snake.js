f3d18f78ad8ff6e722dc3b951359750fa44f7fa2
Thiago Melo <41451201+tbgdmelo@users.noreply.github.com>
2021-05-22 10:08:34 -0300

    pause do game e novo modo p gerar cor de alimento

--- snake/js/snake.js
+++ snake/js/snake.js
@@ -6,34 +6,41 @@
     let snake;
     let placar;
     let alimento;
+    let pausar=false;
+    let perdeu=false;
+    let comecou=false; //pra não executar duas vezes a inicializacao
     
     function init() {
         placar = new Placar();
         board = new Board();
         snake = new Snake();
         alimento = new Alimento();
-
-        //esperando para iniciar
-        window.addEventListener("keydown", function(e){
-            if(e.key==='s'){
-                window.setInterval(run, 300/FPS);
-                window.setInterval(verificaAlimento, 300/FPS);
-            }
-            else{
-                
-            }
-        });
+        
     }
+
+    function comeca(){
+        if(!comecou){
+            comecou=true;
+            window.setInterval(run, 300/FPS);
+            window.setInterval(verificaAlimento, 300/FPS);
+        }
+    }
+
+    window.addEventListener("keydown", function(e){
+        if(e.key==='s'){
+            comeca();
+        }
+    });
 
     window.addEventListener("keydown", function(e) {
         switch(e.key) {            
             case "ArrowUp":
-                if(snake.direcao!==2){
+                if(snake.direcao!==2){ //baixo
                     snake.mudarDirecao(0);
                 }
                 break;
             case "ArrowRight":
-                if(snake.direcao!==3){
+                if(snake.direcao!==3){ //esquerda
                     snake.mudarDirecao(1);
                 }
                 break;
@@ -50,14 +57,59 @@
         }
     });
 
+    function pause(){
+        console.log('hora de parar')
+        if(!pausar){
+            pausar=true;
+        }
+        else if(pausar){
+            pausar=false;
+        }
+    }
+
+    window.addEventListener('keydown', function(e){
+        if(e.key==='p'){
+            pause();
+        }
+    });
+
     class Alimento{
         constructor(){
             //Preciso de uma posição diferente das que são ocupadas pela snake
             let posicao= snake.getPosLivre();
-            this.local = [posicao];
-            this.cor = "#111111";
+            this.local = [posicao]; 
+            this.cor = this.sorteiaCor();
             this.local.forEach(campo => document.querySelector(`#board tr:nth-child(${campo[0]}) td:nth-child(${campo[1]})`).style.backgroundColor = this.cor);            
         }
+
+        sorteiaCor(){
+            let coresAlimento=["#111111","#111111","#ff0000"];
+            const random = (min, max) =>Math.floor(Math.random() * (max-min) +min);
+            return coresAlimento[random(0,3)];
+        }
+        defineCor(){
+            let possibilidades =["#111111","#ff0000"];
+            let probabilidades = [0.66, 0.34];
+    
+            let total = 0;
+            const random = (min, max) => Math.floor(Math.random() * (max-min) +min);
+            let chanceSorteada = random(0,2) ;
+    
+            for (let j = 0; j < possibilidades.length; j++) {
+                total += probabilidades[j];
+                
+                console.log(chanceSorteada);
+                console.log(total);
+    
+                if (chanceSorteada <= total) {
+                    console.log(possibilidades[j]);
+                    return possibilidades[j];
+                    break;
+                }
+            }
+            
+        }
+
     }
 
     class Placar{
@@ -71,7 +123,7 @@
             placarPontos.setAttribute('id','pontuacao');
 
             // Criar o nó de texto
-            var texto = document.createTextNode(this.pontos);
+            var texto = document.createTextNode(this.pontos.toLocaleString('en', {minimumIntegerDigits:5,useGrouping:false}));
 
             // Anexar o nó de texto ao elemento h1
             placarPontos.appendChild(texto);
@@ -133,15 +185,15 @@
             this.direcao = direcao;
         }
 
-        //Gerar um x e um y que não seja ocupado pela snake 
+        //Gerar um x e um y que não seja ocupado pela snake
         getPosLivre(){
             let x=4;
             let y=5;
             let ocupado=true;
                 
             while(ocupado){
-                x=Math.floor(Math.random() * TAM + 1);
-                y=Math.floor(Math.random() * TAM + 1);
+                x=Math.floor(Math.random() * TAM + 1); //4
+                y=Math.floor(Math.random() * TAM + 1); //6
                 for (var i = 0; i < snake.corpo.length; i++) {
                     if(x===snake.corpo[i][0] && y===snake.corpo[i][1]){
                         console.log('posicao invalida foi: '+x,y);
@@ -159,13 +211,21 @@
 
     function verificaAlimento(){
         //cabeça é o último elemento
-        let cabeca = snake.corpo[snake.corpo.length-1];
+        let cabeca = snake.corpo[snake.corpo.length-1];//[5,6]  [5,6]
 
         if(cabeca[0] == alimento.local[0][0] && cabeca[1] == alimento.local[0][1]){
-            placar.pontos = placar.pontos+1;
-            console.log(placar.pontos);
 
-            document.getElementById("pontuacao").innerHTML = placar.pontos;
+            if(alimento.cor==='#ff0000'){
+                placar.pontos = placar.pontos+2;
+            }
+            else{
+                placar.pontos = placar.pontos+1;
+            }
+            
+            //console.log(placar.pontos);
+
+            document.getElementById("pontuacao").innerHTML = placar.pontos.toLocaleString('en', {minimumIntegerDigits:5,useGrouping:false});
+
             //Aumentar tamanho
             //Vou adicionar no incio da lista para a snake crescer para a cauda
             let cauda = snake.corpo[0];
@@ -177,7 +237,9 @@
     }
 
     function run () {
-        snake.andar();
+        if(!pausar){
+            snake.andar();
+        }
     }
 
     init();
